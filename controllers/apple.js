ec2e90bd35d41f69415b91199865b63e7bf09084
Venkata Gopi Siva Sai Nallapati <sainallapati55@gmail.com>
2021-04-09 14:54:00 -0500

    modified

--- controllers/apple.js
+++ controllers/apple.js
@@ -13,10 +13,20 @@ exports.apple_list = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
-// for a specific apple.s
-exports.apple_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: apple detail: ' + req.params.id);
+
+// for a specific apple.
+exports.apple_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await apple.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
+
 // Handle apple create on POST.
 exports.apple_create_post = async function(req, res) {
     console.log(req.body)
@@ -41,8 +51,21 @@ exports.apple_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: apple delete DELETE ' + req.params.id);
 };
 // Handle apple update form on PUT.
-exports.apple_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Apple update PUT' + req.params.id);
+exports.apple_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await apple.findById( req.params.id)
+        if(req.body.applename) toUpdate.applename = req.body.applename;
+        if(req.body.color) toUpdate.color = req.body.color;
+        if(req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 // VIEWS
 // Handle a show all view
