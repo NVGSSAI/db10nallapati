4099b3f625491985748917823e4719cff3805a5c
Venkata Gopi Siva Sai Nallapati <sainallapati55@gmail.com>
2021-04-13 00:02:39 -0500

    modified

--- controllers/apple.js
+++ controllers/apple.js
@@ -47,9 +47,18 @@ exports.apple_create_post = async function(req, res) {
     }
 };
 // Handle apple delete form on DELETE.
-exports.apple_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: apple delete DELETE ' + req.params.id);
+exports.apple_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await apple.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
 // Handle apple update form on PUT.
 exports.apple_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body 
@@ -78,3 +87,56 @@ exports.apple_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+// Handle a show one view with id specified by query
+exports.apple_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await apple.findById( req.query.id)
+        res.render('appledetail', 
+{ title: 'apple Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.apple_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('applecreate', { title: 'apple Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a costume.
+// query provides the id
+exports.apple_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await apple.findById(req.query.id)
+        res.render('appleupdate', { title: 'apple Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle a delete one view with id from query
+exports.apple_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await apple.findById(req.query.id)
+        res.render('appledelete', { title: 'Apple Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
--- routes/apples.js
+++ routes/apples.js
@@ -4,5 +4,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', apple_controlers.apple_view_all_Page );
+/* GET detail apple page */
+router.get('/detail', apple_controlers.apple_view_one_Page);
+/* GET create apple page */
+router.get('/create', apple_controlers.apple_create_Page);
+/* GET create update page */
+router.get('/update', apple_controlers.apple_update_Page);
+/* GET create costume page */
+router.get('/delete', apple_controlers.apple_delete_Page);
+
+
+
+
 
 module.exports = router;
