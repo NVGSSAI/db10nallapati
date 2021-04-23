cec0a5c3584737faec85946c548923f275e5bbee
Venkata Gopi Siva Sai Nallapati <sainallapati55@gmail.com>
2021-04-23 16:18:00 -0500

    modified

--- routes/apples.js
+++ routes/apples.js
@@ -3,13 +3,23 @@ const apple_controlers= require('../controllers/apple');
 var router = express.Router();
 
 /* GET home page. */
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 router.get('/', apple_controlers.apple_view_all_Page );
 /* GET detail apple page */
 router.get('/detail', apple_controlers.apple_view_one_Page);
 /* GET create apple page */
 router.get('/create', apple_controlers.apple_create_Page);
 /* GET create update page */
-router.get('/update', apple_controlers.apple_update_Page);
+router.get('/update',secured, apple_controlers.apple_update_Page);
 /* GET create costume page */
 router.get('/delete', apple_controlers.apple_delete_Page);
 
